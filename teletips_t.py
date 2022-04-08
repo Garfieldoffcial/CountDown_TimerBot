4dae0f9c5d47049174d5d00dc9a0015da933bb5f
Garfield Offcial <100839035+Garfieldoffcial@users.noreply.github.com>
2022-04-08 06:55:40 +0530

    Add files via upload

---
+++ teletips_t.py
@@ -0,0 +1,40 @@
+#Copyright ©️ 2022 Bot Proffer. All Rights Reserved
+#You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
+# ##Credits - [Countdown Timer Telegram bot by Bot Officer] (@Dr_Stranger_Botz_Infinity)
+
+# Changing the code is not allowed!
+                       
+  
+START_TEXT = 'Heya,\nMy name is **Countdown Timer** ⏰\n\nI can countdown to your important events in **any group chat**. 🎉\nUse me to keep track of how much time is left for the event ⏳\n\nHit **❓HELP** to find out more about how to use me.'
+
+GROUP_TEXT="""
+👥 <u>**Help & Support Groups**</u>
+> Feel free to report any issues regarding this service.
+"""
+
+HELP_TEXT = """
+<u>**Step 1**</u> - **Get Started** 🚀
+> Add your bot to your group.
+
+<u>**Step 2**</u> - **Make Admin** 👮🏻‍♂️
+> Don't forget to make your bot admin of the group.
+
+<u>**Step 3**</u> - **Commands** ( /set  &  /stopc )🕹
+> Send the command below in correct format to the group.
+
+<code>/set seconds "event"</code>
+
+**Example**:
+<code>/set 86400 "TIME LEFT UNTIL NEW YEAR"</code>
+
+> You can send <code>/stopc</code> to stop all countdowns. 
+
+That's all! 🥳
+
+**⚠️WARNING:**
+DO NOT do more than two (2) countdowns at the same time using the same bot. (Reason: Telegram floodwait)
+"""
+
+TUTORIAL_TEXT = "💡 Click the **🎥 Video** button to watch how to create this bot for your group."
+
+#Copyright ©️ 2021 Bot Officer . All Rights Reserved.
